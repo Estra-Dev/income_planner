07245da1f13b4c238480ff0342d948f0e55180ea
Estra-Dev <dominikolyson@gmail.com>
2025-01-23 17:29:10 +0100

    add persist reducer to permanently keep user details

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
-// import {persistStore, persistReducer} from "redux-persist"
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    name: userReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleWare) =>
+    getDefaultMiddleWare({ serializableCheck: false }),
 });
+
+export const persistor = persistStore(store);
